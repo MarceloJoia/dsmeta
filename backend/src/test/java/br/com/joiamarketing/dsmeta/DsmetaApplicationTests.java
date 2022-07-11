3cc3b7a1d9192ac01a6cb9b67a399f63f0483307
MarceloJoia <sitejoia@hotmail.com>
2022-07-11 11:33:24 -0300

    Project created

---
+++ backend/src/test/java/br/com/joiamarketing/dsmeta/DsmetaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.joiamarketing.dsmeta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsmetaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
